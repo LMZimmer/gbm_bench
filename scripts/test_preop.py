002fb7d3a2c8b54120d08d625a473cf9f5c072d2
LZimmer <zimmer.lucas.w@gmail.com>
2025-02-12 16:03:40 +0100

    Create test script

---
+++ scripts/test_preop.py
@@ -0,0 +1,12 @@
+from gbm_bench.preprocessing.preprocess import preprocess_dicom
+
+
+if __name__=="__main__":
+    preprocess_dicom(
+            t1="test_data/exam1/t1",
+            t1c="test_data/exam1/t1c",
+            t2="test_data/exam1/t2",
+            flair="test_data/exam1/flair",
+            pre_treatment=True,
+            gpu_device="2"
+            )
