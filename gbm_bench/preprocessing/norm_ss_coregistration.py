debb39843dbe76b608f0dc3acb5dbcf4102aa426
LZimmer <zimmer.lucas.w@gmail.com>
2025-02-20 13:53:00 +0100

    Rename registration functions Add longitudinal co-registration

--- gbm_bench/preprocessing/norm_ss_coregistration.py
+++ gbm_bench/preprocessing/norm_ss_coregistration.py
@@ -1,10 +1,10 @@
-import argparse
 import os
-from brainles_preprocessing.modality import Modality, CenterModality
-from brainles_preprocessing.normalization.percentile_normalizer import (
-    PercentileNormalizer, 
-)
+import ants
+import shutil
+import argparse
 from brainles_preprocessing.preprocessor import Preprocessor
+from brainles_preprocessing.modality import Modality, CenterModality
+from brainles_preprocessing.normalization.percentile_normalizer import PercentileNormalizer
 
 
 def initialize_center_modality(modality_path, modality_name, normalizer, outdir):
@@ -41,7 +41,7 @@ def initialize_moving_modalities(modality_paths, modality_names, normalizer, out
     return moving_modalities
 
 
-def run_preprocessing(t1, t1c, t2, flair, outdir):
+def norm_ss_coregister(t1, t1c, t2, flair, outdir):
 
     percentile_normalizer = PercentileNormalizer(
             lower_percentile=0.1,
@@ -72,7 +72,68 @@ def run_preprocessing(t1, t1c, t2, flair, outdir):
     preprocessor.run()
 
 
+def register_longitudinal(t1c_pre_dir, t1c_post_dir, outdir, transform_images_dict, transform_masks_dict):
+    t1c_pre = ants.image_read(t1c_pre_dir)
+    t1c_post = ants.image_read(t1c_post_dir)
+
+    reg = ants.registration(
+            fixed=t1c_pre,
+            moving=t1c_post,
+            type_of_transform="SyN",
+            reg_iterations=(50, 20),
+            shrink_factors=(2, 1),
+            smoothing_sigmas=(1, 0)
+            )
+
+    os.makedirs(outdir, exist_ok=True)
+    #ants.write_transform(reg["warpedmovout"], os.path.join(outdir, "longitudinal_trafo.mat"))
+    shutil.copyfile(src=reg["fwdtransforms"][0], dst=os.path.join(outdir, "longitudinal_trafo.mat"))
+    ants.image_write(reg["warpedmovout"], os.path.join(outdir, "t1c_warped_longitudinal.nii.gz"))
+
+    for img_id, img_dir in transform_images_dict.items():
+        img = ants.image_read(img_dir)
+        img_warped = ants.apply_transforms(
+                fixed=t1c_pre,
+                moving=img,
+                transformlist=reg["fwdtransforms"],
+                interpolator='linear'
+                )
+        ants.image_write(img_warped, os.path.join(outdir, f"{img_id}_warped_longitudinal.nii.gz"))
+
+    for mask_id, mask_dir in transform_masks_dict.items():
+        mask = ants.image_read(mask_dir)
+        mask_warped = ants.apply_transforms(
+                fixed=t1c_pre,
+                moving=mask,
+                transformlist=reg["fwdtransforms"],
+                interpolator='nearestNeighbor'
+                )
+        ants.image_write(mask_warped, os.path.join(outdir, f"{img_id}_warped_longitudinal.nii.gz"))
+
+
 if __name__ == "__main__":
+    # python gbm_bench/preprocessing/norm_ss_coregistration.py
+    os.environ["CUDA_VISIBLE_DEVICES"] = "4"
+    t1c_pre_dir = "/home/home/lucas/projects/gbm_bench/test_data/exam1/preprocessing/skull_stripped/t1c_bet_normalized.nii.gz"
+    t1c_post_dir = "/home/home/lucas/projects/gbm_bench/test_data/exam2/preprocessing/skull_stripped/t1c_bet_normalized.nii.gz"
+    outdir = "/home/home/lucas/projects/gbm_bench/test_data/exam2/preprocessing/longitudinal/"
+    transform_images_dict = {
+            "t1": "/home/home/lucas/projects/gbm_bench/test_data/exam2/preprocessing/skull_stripped/t1_bet_normalized.nii.gz",
+            "t2": "/home/home/lucas/projects/gbm_bench/test_data/exam2/preprocessing/skull_stripped/t2_bet_normalized.nii.gz",
+            "flair": "/home/home/lucas/projects/gbm_bench/test_data/exam2/preprocessing/skull_stripped/flair_bet_normalized.nii.gz"
+            }
+    transform_masks_dict = {
+            "tumor_seg": "/home/home/lucas/projects/gbm_bench/test_data/exam2/preprocessing/tumor_segmentation/tumor_seg.nii.gz"
+            }
+    register_longitudinal(
+            t1c_pre_dir=t1c_pre_dir,
+            t1c_post_dir=t1c_post_dir,
+            outdir=outdir,
+            transform_images_dict=transform_images_dict,
+            transform_masks_dict=transform_masks_dict
+            )
+
+    """
     # Example:
     # python gbm_bench/preprocessing/norm_ss_coregistration.py -t1 test_data/exam1/preprocessing/nifti_conversion/t1.nii.gz -t1c test_data/exam1/preprocessing/nifti_conversion/t1c.nii.gz -t2 test_data/exam1/preprocessing/nifti_conversion/t2.nii.gz -flair test_data/exam1/preprocessing/nifti_conversion/flair.nii.gz -outdir tmp_test_ss/
     parser = argparse.ArgumentParser()
@@ -86,10 +147,31 @@ if __name__ == "__main__":
 
     os.environ["CUDA_VISIBLE_DEVICES"] = args.cuda_device
     
-    run_preprocessing(
+    norm_ss_coregister(
             t1=args.t1,
             t1c=args.t1c,
             t2=args.t2,
             flair=args.flair,
             outdir=args.outdir
             )
+
+    # Testing register_longitudinal
+    t1c_pre_dir = "/home/home/lucas/projects/gbm_bench/test_data/exam1/preprocessing/skull_stripped/t1c_bet_normalized.nii.gz"
+    t1c_post_dir = "/home/home/lucas/projects/gbm_bench/test_data/exam2/preprocessing/skull_stripped/t1c_bet_normalized.nii.gz"
+    outdir = "/home/home/lucas/projects/gbm_bench/test_data/exam2/preprocessing/longitudinal/"
+    transform_images_dict = {
+            "t1": "/home/home/lucas/projects/gbm_bench/test_data/exam2/preprocessing/skull_stripped/t1_bet_normalized.nii.gz",
+            "t2": "/home/home/lucas/projects/gbm_bench/test_data/exam2/preprocessing/skull_stripped/t2_bet_normalized.nii.gz",
+            "flair": "/home/home/lucas/projects/gbm_bench/test_data/exam2/preprocessing/skull_stripped/flair_bet_normalized.nii.gz"
+            }
+    transform_masks_dict = {
+            "tumor_seg": "/home/home/lucas/projects/gbm_bench/test_data/exam2/preprocessing/tumor_segmentation/tumor_seg.nii.gz"
+            }
+    register_longitudinal(
+            t1c_pre_dir=t1c_pre_dir,
+            t1c_post_dir=t1c_post_dir,
+            outdir=outdir,
+            transform_images_dict=transform_images_dict,
+            transform_masks_dict=transform_masks_dict
+            )
+    """
--- gbm_bench/preprocessing/preprocess.py
+++ gbm_bench/preprocessing/preprocess.py
@@ -3,11 +3,11 @@ import datetime
 from gbm_bench.utils.utils import timed_print
 from gbm_bench.preprocessing.dicom_to_nifti import niftiConvert
 from gbm_bench.preprocessing.tumor_segmentation import run_brats
-from gbm_bench.preprocessing.norm_ss_coregistration import run_preprocessing
+from gbm_bench.preprocessing.norm_ss_coregistration import norm_ss_coregister
 from gbm_bench.preprocessing.tissue_segmentation import generate_healthy_brain_mask, run_tissue_seg_registration
 
 
-def preprocess_dicom(t1, t1c, t2, flair, dcm2niix_location, pre_treatment=True, cuda_device="2"):
+def preprocess_dicom(t1, t1c, t2, flair, dcm2niix_location, pre_treatment, cuda_device="2", perform_nifti_conversion=True, perform_skullstripping=True, perform_tumorseg=True, perform_tissueseg=True):
 
     os.environ["CUDA_VISIBLE_DEVICES"] = cuda_device
 
@@ -25,12 +25,13 @@ def preprocess_dicom(t1, t1c, t2, flair, dcm2niix_location, pre_treatment=True, 
         outdir = os.path.join(os.path.dirname(dicom_folder), "preprocessing")
         nifti_dir = os.path.join(outdir, "nifti_conversion")
 
-        niftiConvert(
-                input_dir=dicom_folder,
-                export_dir=nifti_dir,
-                outfile=modality_name,
-                dcm2niix_location=dcm2niix_location
-                )
+        if perform_nifti_conversion:
+            niftiConvert(
+                    input_dir=dicom_folder,
+                    export_dir=nifti_dir,
+                    outfile=modality_name,
+                    dcm2niix_location=dcm2niix_location
+                    )
 
     timed_print("Finished DICOM to NIfTI conversion.")
 
@@ -39,13 +40,14 @@ def preprocess_dicom(t1, t1c, t2, flair, dcm2niix_location, pre_treatment=True, 
     timed_print("Starting normalization, co-registration and skull stripping...")
     preprocessed_dir = os.path.join(outdir, "skull_stripped")
 
-    run_preprocessing(
-            t1=os.path.join(nifti_dir, "t1.nii.gz"),
-            t1c=os.path.join(nifti_dir, "t1c.nii.gz"),
-            t2=os.path.join(nifti_dir, "t2.nii.gz"),
-            flair=os.path.join(nifti_dir, "flair.nii.gz"),
-            outdir=preprocessed_dir
-            )
+    if perform_skullstripping:
+        norm_ss_coregister(
+                t1=os.path.join(nifti_dir, "t1.nii.gz"),
+                t1c=os.path.join(nifti_dir, "t1c.nii.gz"),
+                t2=os.path.join(nifti_dir, "t2.nii.gz"),
+                flair=os.path.join(nifti_dir, "flair.nii.gz"),
+                outdir=preprocessed_dir
+                )
 
     timed_print("Finished normalization, co-registration and skull stripping.")
 
@@ -56,15 +58,16 @@ def preprocess_dicom(t1, t1c, t2, flair, dcm2niix_location, pre_treatment=True, 
     tumor_outfile = os.path.join(tumor_outdir, "tumor_seg.nii.gz")
     os.makedirs(tumor_outdir, exist_ok=True)
 
-    run_brats(
-            t1=os.path.join(preprocessed_dir, "t1_bet_normalized.nii.gz"),
-            t1c=os.path.join(preprocessed_dir, "t1c_bet_normalized.nii.gz"),
-            t2=os.path.join(preprocessed_dir, "t2_bet_normalized.nii.gz"),
-            flair=os.path.join(preprocessed_dir, "flair_bet_normalized.nii.gz"),
-            outfile=tumor_outfile,
-            pre_treatment=pre_treatment,
-            cuda_device="2"
-            )
+    if perform_tumorseg:
+        run_brats(
+                t1=os.path.join(preprocessed_dir, "t1_bet_normalized.nii.gz"),
+                t1c=os.path.join(preprocessed_dir, "t1c_bet_normalized.nii.gz"),
+                t2=os.path.join(preprocessed_dir, "t2_bet_normalized.nii.gz"),
+                flair=os.path.join(preprocessed_dir, "flair_bet_normalized.nii.gz"),
+                outfile=tumor_outfile,
+                pre_treatment=pre_treatment,
+                cuda_device=cuda_device
+                )
 
     timed_print("Finished tumor segmentation.")
 
@@ -81,13 +84,14 @@ def preprocess_dicom(t1, t1c, t2, flair, dcm2niix_location, pre_treatment=True, 
             )
 
     tissue_seg_dir = os.path.join(outdir, "tissue_segmentation")
-    run_tissue_seg_registration(
-            t1_file=os.path.join(preprocessed_dir, "t1c_bet_normalized.nii.gz"),
-            healthy_mask_dir=healthy_mask_dir,
-            brain_mask_dir=brain_mask_dir,
-            outdir=tissue_seg_dir,
-            refit_brain=False
-            )
+    if perform_tissueseg:
+        run_tissue_seg_registration(
+                t1_file=os.path.join(preprocessed_dir, "t1c_bet_normalized.nii.gz"),
+                healthy_mask_dir=healthy_mask_dir,
+                brain_mask_dir=brain_mask_dir,
+                outdir=tissue_seg_dir,
+                refit_brain=False
+                )
 
     timed_print("Finished tissue segmentation.")
 
@@ -97,22 +101,23 @@ if __name__ == "__main__":
     # python gbm_bench/preprocessing/preprocess.py
 
     # Pre-treatment example
-    preprocess_dicom(
-            t1="test_data/exam1/t1",
-            t1c="test_data/exam1/t1c",
-            t2="test_data/exam1/t2",
-            flair="test_data/exam1/flair",
-            dcm2niix_location="/home/home/lucas/bin/dcm2niix",
-            pre_treatment=True,
-            cuda_device="4"
-            )
+    #preprocess_dicom(
+    #        t1="test_data/exam1/t1",
+    #        t1c="test_data/exam1/t1c",
+    #        t2="test_data/exam1/t2",
+    #        flair="test_data/exam1/flair",
+    #        dcm2niix_location="/home/home/lucas/bin/dcm2niix",
+    #        pre_treatment=True,
+    #        cuda_device="4"
+    #        )
     
     # Post-treatment example
-    #preprocess_dicom(
-    #        t1="test_data/exam2/t1",
-    #        t1c="test_data/exam2/t1c",
-    #        t2="test_data/exam2/t2",
-    #        flair="test_data/exam2/flair",
-    #        pre_treatment=False,
-    #        cuda_device="2"
-    #        )
+    preprocess_dicom(
+            t1="test_data/exam2/t1",
+            t1c="test_data/exam2/t1c",
+            t2="test_data/exam2/t2",
+            flair="test_data/exam2/flair",
+            pre_treatment=False,
+            perform_tissueseg=False,
+            cuda_device="4"
+            )
