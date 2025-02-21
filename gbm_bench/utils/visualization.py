a4ecfc02692cc09ea31e5ec0798a4a93908b787f
LZimmer <zimmer.lucas.w@gmail.com>
2025-02-21 11:35:29 +0100

    Nicer plots

--- gbm_bench/utils/visualization.py
+++ gbm_bench/utils/visualization.py
@@ -3,6 +3,7 @@ import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
+import matplotlib.patches as mpatches
 from typing import Dict, List, Tuple
 from gbm_bench.utils.utils import compute_center_of_mass, load_mri_data, load_and_resample_mri_data, merge_pdfs
 
@@ -17,9 +18,9 @@ def get_image_dirs(preprocessing_dir: str) -> Dict:
             "tumorseg": os.path.join(preprocessing_dir, "tumor_segmentation/tumor_seg.nii.gz"),
             "tissueseg": [
                 os.path.join(preprocessing_dir, "tissue_segmentation/tissue_seg.nii.gz"),
-                os.path.join(preprocessing_dir, "tissue_segmentation/wm.nii.gz"),
-                os.path.join(preprocessing_dir, "tissue_segmentation/gm.nii.gz"),
-                os.path.join(preprocessing_dir, "tissue_segmentation/csf.nii.gz")
+                os.path.join(preprocessing_dir, "tissue_segmentation/wm_pbmap.nii.gz"),
+                os.path.join(preprocessing_dir, "tissue_segmentation/gm_pbmap.nii.gz"),
+                os.path.join(preprocessing_dir, "tissue_segmentation/csf_pbmap.nii.gz")
                 ],
             "lmi": os.path.join(preprocessing_dir, "lmi/lmi_tumor_patientSpace.nii"),
             "masks": [
@@ -38,7 +39,7 @@ def plot_mri_with_segmentation(
     algorithm_identifier: str,
     preprocessing_dir: str,
     outfile: str,
-    classes_of_interest: List[int] = [1, 2, 3],
+    classes_of_interest: List[int] = [1, 2, 3]
 ) -> None:
     
     image_dirs = get_image_dirs(preprocessing_dir)
@@ -56,16 +57,21 @@ def plot_mri_with_segmentation(
         f"Computed center of mass (Axial slice: {slice_num_axial}, Sagittal slice: {slice_num_sagittal})"
     )
 
-    # colormap for tumor segmentation (none for background)
-    colors = ["none", "green", "blue", "red"]
+    # colormap for tumor segmentation (1: non enhancing, 2: edema, 3: enhancing)
+    colors = [(0,0,0,0), (1, 127/255, 0, 1), (30/255, 144/255, 1, 1), (138/255, 43/255, 226/255, 1)]
+    color_labels = ["Non-enhancing Tumor", "Peritumoral Edema", "Enhancing Tumor"]
     cmap = mcolors.ListedColormap(colors)
-    bounds = [i - 0.5 for i in classes_of_interest] + [classes_of_interest[-1] + 0.5]
+    bounds = [0, 0.5, 1.5, 2.5, 3.5]
     norm = mcolors.BoundaryNorm(bounds, cmap.N)
+    patches = [mpatches.Patch(color=c, label=l) for (c, l) in zip(colors[1:], color_labels)]
 
     # each key-value pair in image_dirs gets 2 rows (axial, sagittal)
     num_sequences = len(image_dirs["stripped"])
     num_rows = len(image_dirs)
     fig, axs = plt.subplots(2 * num_rows, num_sequences, figsize=(20, 8 * num_rows))
+
+    # Threshold for tumor cell concentration in tumor model outputs
+    c_threshold = 0.01
 
     # axial plots
     for i, modality in enumerate(MODALITY_ORDER):
@@ -88,8 +94,8 @@ def plot_mri_with_segmentation(
         # skull strippped + tumor model
         axs[3, i].imshow(np.rot90(load_mri_data(image_dirs["stripped"][i])[:, :, slice_num_axial]), cmap="gray")
         overlay = np.rot90(load_and_resample_mri_data(image_dirs["lmi"], resample_params=patient_dim, interp_type=1)[:, :, slice_num_axial])
-        #overlay = np.rot90(load_mri_data(image_dirs["lmi"])[:, :, slice_num_axial])
-        axs[3, i].imshow(overlay, cmap='inferno',  alpha=0.8)
+        overlay = np.where(overlay < c_threshold, 0, overlay)
+        axs[3, i].imshow(overlay, cmap='inferno',  alpha=0.9, vmin=0.0, vmax=1.0)
         axs[3, i].axis("off")
 
         # masks
@@ -110,14 +116,14 @@ def plot_mri_with_segmentation(
         # skull stripped + tumor segmentation
         axs[7, i].imshow(np.rot90(load_mri_data(image_dirs["stripped"][i])[slice_num_sagittal, :, :]), cmap="gray")
         overlay = np.rot90(seg_data[slice_num_sagittal, :, :])
-        axs[7, i].imshow(overlay, cmap=cmap, norm=norm, alpha=0.8)
+        axs[7, i].imshow(overlay, cmap=cmap, norm=norm, alpha=0.9)
         axs[7, i].axis("off")
 
         # skull strippped + tumor model
         axs[8, i].imshow(np.rot90(load_mri_data(image_dirs["stripped"][i])[slice_num_sagittal, :, :]), cmap="gray")
         overlay = np.rot90(load_and_resample_mri_data(image_dirs["lmi"], resample_params=patient_dim, interp_type=1)[slice_num_sagittal, :, :])
-        #overlay = np.rot90(load_mri_data(image_dirs["lmi"])[slice_num_sagittal, :, :])
-        axs[8, i].imshow(overlay, cmap='inferno', alpha=0.9)
+        overlay = np.where(overlay < c_threshold, 0, overlay)
+        axs[8, i].imshow(overlay, cmap='inferno', alpha=0.9, vmin=0.0, vmax=1.0)
         axs[8, i].axis("off")
 
         # masks
@@ -137,13 +143,24 @@ def plot_mri_with_segmentation(
     # Add identifiers with adjusted margins and bounding box
     fig.subplots_adjust(top=0.85)
     fig.suptitle(
-            f"Patient: {patient_identifier}\nExam: {exam_identifier}\nAlgorithm: {algorithm_identifier}\nSlice (axial/sagittal): {slice_num_axial}/{slice_num_sagittal}",
-        fontsize=20,
-        fontweight="bold",
-        color="black",
-        y=0.95,
-        bbox=dict(facecolor="white", edgecolor="none", alpha=0.7),
-    )
+            (
+                f"Patient: {patient_identifier}\n"
+                f"Exam: {exam_identifier}\n"
+                f"Algorithm: {algorithm_identifier}\n"
+                f"Slice (axial/sagittal): {slice_num_axial}/{slice_num_sagittal}\n"
+                f"Tumor cell concentration threshold: {c_threshold}"
+                ),
+            horizontalalignment="left",
+            fontsize=20,
+            fontweight="bold",
+            color="black",
+            y=0.92,
+            x=0.066,
+            bbox=dict(facecolor="white", edgecolor="none", alpha=0.7),
+            )
+
+    # Color legends
+    fig.legend(handles=patches, loc="upper right", bbox_to_anchor=(0.96, 0.891), ncol=3)
 
     plt.tight_layout(rect=[0, 0, 1, 0.9])
     plt.savefig(outfile, format="pdf")
